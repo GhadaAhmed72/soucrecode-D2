e63b98080f5cbe374a6bcf54fbc53132db2e6a86
alaa-elsayed <alaaelsayed7152@gemail.com>
2025-05-25 13:34:54 +0200

    add form

--- script.js
+++ script.js
@@ -4,4 +4,26 @@ alertButton.addEventListener("click" , function(){
     alert("welcome to source control course");
     
 })
+let form=document.querySelector(".form");
+form.innerHTML=`  <h2 class="text-center">Form 1</h2>  <form class="w-75 mx-auto">
+      <div class="mb-3">
+        <label for="name" class="form-label">Full Name</label>
+        <input type="text" class="form-control" id="name" placeholder="Enter your full name">
+      </div>
 
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="email" placeholder="name@example.com">
+      </div>
+
+      <div class="mb-3">
+        <label for="message" class="form-label">Your Message</label>
+        <textarea class="form-control" id="message" rows="4" placeholder="Write your message here..."></textarea>
+      </div>
+
+      <button type="submit" class="btn btn-primary">Send</button>
+    </form>`
+let submit=document.querySelector(".btn")
+submit.addEventListener("click",()=>{
+    alert("hello user")
+})
